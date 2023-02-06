62ac55bffbce817245a748081aaef3799a0b68e0
Tidders <tidders1221@gmail.com>
2023-02-06 11:36:21 +0000

    fixed it innit

--- mashup/index.js
+++ mashup/index.js
@@ -1,8 +1,8 @@
-const a = 'b';
-console.log('Hello this is mashup js')
+//const a = 'b';
+console.log("Hello this is mashup js");
 
-if (myStatement = false)
+myStatement = true;
 
-{
-  console.log('This is mashup' + 'strings'); if (true == false) { }
+if (myStatement = false) {
+  console.log("This is mashup" + "strings");
 }
