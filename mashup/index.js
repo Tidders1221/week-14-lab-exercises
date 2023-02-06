2c503a02b6630fcbe9d14dbd78f25437c123afec
Tidders <tidders1221@gmail.com>
2023-02-06 11:39:27 +0000

    zd

--- mashup/index.js
+++ mashup/index.js
@@ -1,8 +1,8 @@
 //const a = 'b';
 console.log("Hello this is mashup js");
 
-const myStatement = true;
+//const myStatement = true;
 
-if (myStatement = false) {
-  console.log("This is mashup" + "strings");
-}
+//if (myStatement = false) {
+//  console.log("This is mashup" + "strings");
+//}
